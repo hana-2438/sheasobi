dd5ecf8f424747e3a12c45a299d9359585a3acca
hana-2438 <121591703+hana-2438@users.noreply.github.com>
2023-03-10 12:05:35 +0000

    管理者トップページ(投稿一覧)、会員一覧画面

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,8 +1,10 @@
 class Admin::MembersController < ApplicationController
   def index
+    @members = Member.all.page(params[:page]).per(6)
   end
 
   def edit
+    @member = Member.find(params[:id])
   end
 
   def update
