f77dd55b7144c8d3e9cf0ad405b648ce6729558a
hana-2438 <121591703+hana-2438@users.noreply.github.com>
2023-03-18 12:44:41 +0000

    [add]production.rbにメールの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,17 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+   # お問い合わせgmail送信用の設定
+  config.action_mailer.raise_delivery_errors  = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:                      'smtp.gmail.com',
+    port:                         587,
+    domain:                       'gmail.com',
+    user_name:                    ENV['INQUIRIES_EMAIL'],
+    password:                     ENV['INQUIRIES_PASSWORD'],
+    authentication:               'plain',
+    enable_starttls_auto:         true
+  }
 end
